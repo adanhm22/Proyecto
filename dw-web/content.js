d7eec2eb1c2475fe9ca35b983549fb9bdab07415
adanhm22 <43605705+adanhm22@users.noreply.github.com>
2019-05-28 23:26:06 +0200

    cosas varias
    reorganizado el codigo, algunas cosas ahora funcionan, la funcionalidad, la misma de hace una semana

--- dw-web/content.js
+++ dw-web/content.js
@@ -1,26 +1,43 @@
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
-   if (request.message == "dom")
+class Utiles
+{
+   serializar (dom)
    {
-      let respuesta;
-      console.log(document)
-      chrome.runtime.sendMessage({message: "transform_doc",data : document},function (response){
-         respuesta = response(document);
-      });
-      dom = new XMLSerializer().serializeToString(respuesta);
-         descargarPagina(dom,'pagina.html');
-      return true;
-   } 
+      return new XMLSerializer().serializeToString(dom);
+   }
+
+   tratarDom ()
+   {
+      let dom = document.cloneNode(true);
+      console.log(a[0].parentNode);
       
- });
+      // pasos: necesito descargar todas las imagenes, ponerlas en una carpeta
+      // y todos los link pasarlos a su respectivo archivo en esa carpeta.
 
+      return dom;
+   }
 
-   function descargarPagina(dom,nombre)
-    {
+   descargarArchivo(dom,nombre)
+   {
       let blob = new Blob([dom],{type: 'text/plain'});
       let a = document.createElement('a');
       a.download = nombre;
       a.href = window.webkitURL.createObjectURL(blob);//solo funciona en chrome
       a.click();
-    }
- 
+   }
+}
+
+//main
+var utiles = new Utiles();
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
+   if (request.message == "html")
+   {  
+      let dom = utiles.tratarDom();
+      console.log(dom);
+      let pagina = utiles.serializar(dom);
+      utiles.descargarArchivo(pagina,'pagina.html');
+      return true;
+   } 
+      
+ });
+
--- dw-web/popup.js
+++ dw-web/popup.js
@@ -11,7 +11,7 @@ function click() {
         // });
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "dom"});
+        chrome.tabs.sendMessage(activeTab.id, {message: "html"});
         return true;
         });
         // esto ya funciona
